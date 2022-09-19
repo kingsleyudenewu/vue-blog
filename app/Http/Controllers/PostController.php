9387704ae64b58033cf63b23f304ab01f3d0dba2
Kingsley Udenewu <kingzpacking@gmail.com>
2022-09-19 02:46:49 +0100

    Add api resources to format the responses for the post requests through the api.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function myPost()
+    public function __construct(Post $post)
     {
-        
+        $this->post = $post;
+    }
+
+    /**
+     * Fetch all posts
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $posts = $this->post->with('user')
+            ->filterByPublicationDate($request->publication_date)
+            ->paginate(10);
+
+        return $this->successResponse('success', PostResource::collection($posts)->response());
+    }
+
+    /**
+     * Fetch only post belonging to a user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function myPost(): \Illuminate\Http\JsonResponse
+    {
+        $posts = $this->post->where('user_id', auth()->user()->id)
+            ->latest()
+            ->paginate(10);
+
+        return $this->successResponse('success', PostResource::collection($posts)->response());
+    }
+
+
+    public function create(CreatePostRequest $request)
+    {
+        $payload = collect($request->validated())->merge(['user_id' => auth()->user()->id])->all();
+
+        $post = $this->post->create($payload);
+
+        return $this->createdResponse('Post created successfully', $post);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilterByPublicationDate(Builder $query, string $publicationDate = null)
+    {
+        return $query->orderBy('created_at', 'desc')
+            ->when(!empty($publicationDate), function($query) use ($publicationDate) {
+                return $query->orderBy('published_at', $publicationDate);
+            });
+    }
+
+    public function getPublishedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
 }
