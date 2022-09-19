8814d42afed54a8c854d28a34045528cfeab732d
Kingsley Udenewu <kingzpacking@gmail.com>
2022-09-19 02:39:57 +0100

    Add auth controller to authenticate a user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,29 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\GenerateTokenAction;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest $request)
+    /**
+     * Register a user into the blog system
+     *
+     * @param RegisterRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request): \Illuminate\Http\JsonResponse
     {
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
+            'role' => User::USER
         ]);
 
-        $token = $user->createToken("API TOKEN")->plainTextToken;
+        $payload = (new GenerateTokenAction())->generatePayload($user);
 
-        return $this->createdResponse("User created successfully", $token);
+        return $this->createdResponse("User created successfully", $payload);
     }
 
-    public function login(LoginRequest $request)
+    /**
+     * Login a user into the blog system
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request): \Illuminate\Http\JsonResponse
     {
         if(!Auth::attempt($request->only(['email', 'password']))){
             return $this->badRequestAlert('Invalid login credentials');
@@ -32,6 +45,20 @@ class AuthController extends Controller
 
         $user = User::where('email', $request->email)->first();
 
-        return $this->successResponse('User Logged In Successfully', $user->createToken("API TOKEN")->plainTextToken);
+        $payload = (new GenerateTokenAction())->generatePayload($user);
+
+        return $this->successResponse('User Logged In Successfully', $payload);
+    }
+
+    /**
+     * Log user out and destroy token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->successResponse('User successfully logout');
     }
 }
